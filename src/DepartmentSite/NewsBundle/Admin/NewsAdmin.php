a35f345ebef1f54fc41f7b1e81ca7bb632742a0a
Marina Bardiyan <marina.bard.96@gmail.com>
2016-04-16 13:46:51 +0300

    Added CRUD

---
+++ src/DepartmentSite/NewsBundle/Admin/NewsAdmin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace DepartmentSite\NewsBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class NewsAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title', 'text', array('label' => 'Title'))
+            ->add('description', 'text', array('label' => 'Description'))
+            ->add('content', 'text', array('label' => 'Content'))
+            ->add('created_at')
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('title')
+        ;
+    }
+}
---
+++ src/DepartmentSite/NewsBundle/Form/NewsType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace DepartmentSite\NewsBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewsType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('descritpion')
+            ->add('title')
+            ->add('content')
+            ->add('createdAt', 'datetime')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'DepartmentSite\NewsBundle\Entity\News'
+        ));
+    }
+}
