4f81a7689d98b24456339906f11391626c288860
Oxana Kozlova <ooksanakozlova@gmail.com>
2016-06-14 15:43:22 +0300

    origin/ok/feature/evm-42_Install_DefaultBundle   * Add news on main page

--- src/DepartmentSite/DefaultBundle/Controller/DefaultController.php
+++ src/DepartmentSite/DefaultBundle/Controller/DefaultController.php
@@ -11,9 +11,9 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
-        //$news_list = $em->getRepository('DepartmentSiteNewsBundle:News')->findBy(array(), array('id'=>'desc'), 5);
+        $news_list = $em->getRepository('DepartmentSiteNewsBundle:News')->findBy(array(), array('id'=>'desc'), 5);
         $adver_list = $em->getRepository('DepartmentSiteAdvertBundle:Advert')->findBy(array(), array('id'=>'desc'), 5);
         return $this->render('DepartmentSiteDefaultBundle:Default:index.html.twig',
-            array('adverts'=>$adver_list));
+            array('adverts'=>$adver_list, 'news'=>$news_list));
     }
 }
