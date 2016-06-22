625311d09a502c207a3bd724bfd9b25d516ea7e4
Oxana Kozlova <ooksanakozlova@gmail.com>
2016-06-22 18:35:11 +0300

    ok/feature/evm-53-Install_Gallery_Bundle   * Create image admin   * Translate image admin

---
+++ src/DepartmentSite/GalleryBundle/Admin/ImageAdmin.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace DepartmentSite\GalleryBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Ivory\CKEditorBundle\Form\Type\CKEditorType;
+
+class ImageAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('gallery', 'text', array('label' => 'Gallery'))
+           // ->add('image', 'string', array('label' => 'Description'))
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('gallery')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('gallery')
+        ;
+    }
+}
