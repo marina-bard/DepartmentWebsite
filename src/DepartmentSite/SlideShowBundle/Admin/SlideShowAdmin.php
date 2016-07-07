78ddf34532851e70c111cd55cdf627d8150cb441
Marina Bardiyan <marina.bard.96@gmail.com>
2016-07-07 03:35:21 +0300

    Created Slide Show Bundle, added entity and admin class

---
+++ src/DepartmentSite/SlideShowBundle/Admin/SlideShowAdmin.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace DepartmentSite\SlideShowBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use ITM\ImagePreviewBundle\Form\Type\ImagePreviewType;
+
+class SlideShowAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title', 'text', array('label' => 'Title'))
+            ->add('description', 'text', array('label' => 'Description'))
+            ->add('image', ImagePreviewType::class, ['data_class' => null])
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('title')
+            ->add('image', 'string', ['template' => 'DepartmentSiteSlideShowBundle:Admin:list__image.html.twig'] )
+            ->add('_action', null, array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('title')
+            ->add('description')
+            ->add('image')
+        ;
+    }
+}
