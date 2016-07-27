42064edea30919fa57142e4ca3faf5786f304fae
Marina Bardiyan <marina.bard.96@gmail.com>
2016-07-28 00:40:38 +0300

    Changed fos user register template

--- src/Application/FOS/UserBundle/Controller/RegistrationUserController.php
+++ src/Application/FOS/UserBundle/Controller/RegistrationUserController.php
@@ -4,27 +4,46 @@ namespace Application\FOS\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Application\FOS\UserBundle\Form\Type\RegistrationStudentUserFormType;
+use Application\FOS\UserBundle\Form\Type\RegistrationParentUserFormType;
+use Application\FOS\UserBundle\Form\Type\RegistrationTeacherUserFormType;
 
 class RegistrationUserController extends Controller
 {
     public function registerAction()
     {
         $users = [
+            'unsigned' => null,
             'teacher_user' => 'teacher',
             'student_user' => 'student',
             'parent_user' => 'parent'
         ];
+
         return $this->render('ApplicationFOSUserBundle:Registration:choose_user.form.html.twig', array(
             'users' => $users
         ));
+//        return $this->container
+//            ->get('pugx_multi_user.registration_manager')
+//            ->register('Application\FOS\UserBundle\Entity\StudentUser');
     }
 
     public function getFormAction($user)
     {
-        $service = $this->get('fos.user.'.$user);
-        return $this->container
-            ->get('pugx_multi_user.registration_manager')
-            ->register(get_class($service));
+        if(is_null($user)) {
+            return new Response();
+        }
+//        $service = $this->get('fos.user.'.$user);
+//        return $this->container
+//            ->get('pugx_multi_user.registration_manager')
+//            ->register(get_class($service));
+        $form = $this->createForm(RegistrationStudentUserFormType::class);
+////        var_dump($form);
+//        return $this->render('ApplicationFOSUserBundle:Registration:form.html.twig', array(
+//            'form' => $form->createView()
+//        ));
+        return $this->render('FOSUserBundle:Registration:register.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 
     public function registerStudentUserAction()
