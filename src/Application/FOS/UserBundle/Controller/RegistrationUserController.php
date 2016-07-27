0311c7cbf37999675fc8f4ac99665289f2d4d71a
Marina Bardiyan <marina.bard.96@gmail.com>
2016-07-27 12:58:45 +0300

    Added different form rendering to registration form

--- src/Application/FOS/UserBundle/Controller/RegistrationUserController.php
+++ src/Application/FOS/UserBundle/Controller/RegistrationUserController.php
@@ -3,9 +3,30 @@
 namespace Application\FOS\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class RegistrationUserController extends Controller
 {
+    public function registerAction()
+    {
+        $users = [
+            'teacher_user' => 'teacher',
+            'student_user' => 'student',
+            'parent_user' => 'parent'
+        ];
+        return $this->render('ApplicationFOSUserBundle:Registration:choose_user.form.html.twig', array(
+            'users' => $users
+        ));
+    }
+
+    public function getFormAction($user)
+    {
+        $service = $this->get('fos.user.'.$user);
+        return $this->container
+            ->get('pugx_multi_user.registration_manager')
+            ->register(get_class($service));
+    }
+
     public function registerStudentUserAction()
     {
         return $this->container
