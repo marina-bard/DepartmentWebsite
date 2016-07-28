889dca0e6888c4f17db01106ee6ee86550575f7a
ArtuhovichVladislav <artvlad96@gmail.com>
2016-07-28 13:02:08 +0300

    Add form request

--- src/Application/FOS/UserBundle/Controller/RegistrationUserController.php
+++ src/Application/FOS/UserBundle/Controller/RegistrationUserController.php
@@ -2,23 +2,48 @@
 
 namespace Application\FOS\UserBundle\Controller;
 
+use Application\FOS\UserBundle\ApplicationFOSUserBundle;
+use Symfony\Bridge\Doctrine\Tests\Fixtures\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Application\FOS\UserBundle\Form\Type\RegistrationStudentUserFormType;
 use Application\FOS\UserBundle\Form\Type\RegistrationParentUserFormType;
 use Application\FOS\UserBundle\Form\Type\RegistrationTeacherUserFormType;
+use Symfony\Component\HttpFoundation\Request;
 
 class RegistrationUserController extends Controller
 {
-    public function registerAction()
+    /**
+     * Method regi
+     * @param Request $request
+     * @return JsonResponse|Response
+     */
+    public function registerAction(Request $request)
     {
         $users = [
-            'unsigned' => null,
             'teacher_user' => 'teacher',
             'student_user' => 'student',
             'parent_user' => 'parent'
         ];
 
+        $request_student_data = $request->get('fos_user_registration_form_student_user');
+        
+        if (count($request_student_data)) {
+            $form = $this->createForm(RegistrationStudentUserFormType::class);
+            
+            $form->handleRequest($request);
+
+            if ( $form->isValid() && $form->isSubmitted() ) {
+                $student_user = $form->getData();
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($student_user);
+                $em->flush();
+                
+                return new JsonResponse('success');
+            }
+        }
+       
         return $this->render('ApplicationFOSUserBundle:Registration:choose_user.form.html.twig', array(
             'users' => $users
         ));
@@ -27,24 +52,25 @@ class RegistrationUserController extends Controller
 //            ->register('Application\FOS\UserBundle\Entity\StudentUser');
     }
 
-    public function getFormAction($user)
+    public function getFormAction( Request $request)
     {
-        if(is_null($user)) {
-            return new Response();
-        }
-//        $service = $this->get('fos.user.'.$user);
-//        return $this->container
-//            ->get('pugx_multi_user.registration_manager')
-//            ->register(get_class($service));
-        $form = $this->createForm(RegistrationStudentUserFormType::class);
-////        var_dump($form);
-//        return $this->render('ApplicationFOSUserBundle:Registration:form.html.twig', array(
-//            'form' => $form->createView()
-//        ));
+//        if(is_null($user)) {
+//            return new Response();
+//        }
+        $userType = $request->get('type');
+
+        if($userType == 'student')
+            $form = $this->createForm(RegistrationStudentUserFormType::class);
+        else if($userType == 'parent')
+            $form = $this->createForm(RegistrationParentUserFormType::class);
+        else if($userType == 'teacher')
+            $form = $this->createForm(RegistrationTeacherUserFormType::class);
+
         return $this->render('FOSUserBundle:Registration:register.html.twig', array(
             'form' => $form->createView(),
         ));
     }
+
 
     public function registerStudentUserAction()
     {
