ec1d2bb996c994d26a29a78069f9694b7a09d94b
OksanaKozlova <ooksanakozlova@gmail.com>
2017-05-18 18:14:27 +0300

    EVM- fix dictionaty translation value

--- src/DepartmentSite/DictionaryBundle/Entity/DictionaryTranslation.php
+++ src/DepartmentSite/DictionaryBundle/Entity/DictionaryTranslation.php
@@ -17,7 +17,7 @@ class DictionaryTranslation
     /**
      * @var string
      *
-     * @ORM\Column(name="value", type="string", length=255)
+     * @ORM\Column(name="value", type="text")
      */
     private $value;
 
